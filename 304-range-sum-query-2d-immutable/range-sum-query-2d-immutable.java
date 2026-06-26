e465ce8fd679e727f8d0a9c1eb6ccf124c1d4b38
9sindhu <sindhu932005@gmail.com>
2026-06-26 21:39:53 +0530

    Time: 95 ms (99.82%) | Memory: 142.4 MB (14.51%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,4 +1,4 @@
-class NumMatrix {
+ class NumMatrix {
 
     int[][] prefix;
 
@@ -7,41 +7,25 @@ class NumMatrix {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        prefix = new int[m][n];
+        prefix = new int[m + 1][n + 1];
 
-        for(int i = 0; i < m; i++) {
+        for (int i = 1; i <= m; i++) {
 
-            for(int j = 0; j < n; j++) {
+            for (int j = 1; j <= n; j++) {
 
-                int top = (i > 0) ? prefix[i-1][j] : 0;
-
-                int left = (j > 0) ? prefix[i][j-1] : 0;
-
-                int diagonal = (i > 0 && j > 0) ? prefix[i-1][j-1] : 0;
-
-                prefix[i][j] = matrix[i][j] + top + left - diagonal;
+                prefix[i][j] = matrix[i - 1][j - 1]
+                        + prefix[i - 1][j]
+                        + prefix[i][j - 1]
+                        - prefix[i - 1][j - 1];
             }
         }
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
 
-        int total = prefix[row2][col2];
-
-        int top = (row1 > 0) ? prefix[row1-1][col2] : 0;
-
-        int left = (col1 > 0) ? prefix[row2][col1-1] : 0;
-
-        int diagonal = (row1 > 0 && col1 > 0)
-                ? prefix[row1-1][col1-1]
-                : 0;
-
-        return total - top - left + diagonal;
+        return prefix[row2 + 1][col2 + 1]
+                - prefix[row1][col2 + 1]
+                - prefix[row2 + 1][col1]
+                + prefix[row1][col1];
     }
 }
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.sumRegion(row1,col1,row2,col2);
- */
